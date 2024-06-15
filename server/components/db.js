68ed69cee4d0ae1d36bb02b49b8df1a399433f08
shaunak-10 <105604764+shaunak-10@users.noreply.github.com>
2024-06-16 00:10:43 +0530

    Update db.js

--- server/components/db.js
+++ server/components/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const uri =
-  "mongodb+srv://nisarg2411:nisarg2411@knowhow.p4qcwv3.mongodb.net/users?retryWrites=true&w=majority";
+  "mongodb+srv://*:*@knowhow.p4qcwv3.mongodb.net/users?retryWrites=true&w=majority";
 console.log("Connecting to MongoDB");
 
 const con = mongoose
